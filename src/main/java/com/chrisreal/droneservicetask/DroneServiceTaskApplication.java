eafde118a453b3d50cc25841cb8f07f9abfef9e8
chrisimoni <imonireal@gmail.com>
2022-07-09 08:12:39 +0100

    initial commit

---
+++ src/main/java/com/chrisreal/droneservicetask/DroneServiceTaskApplication.java
@@ -0,0 +1,13 @@
+package com.chrisreal.droneservicetask;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DroneServiceTaskApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DroneServiceTaskApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/chrisreal/droneservicetask/DroneServiceTaskApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chrisreal.droneservicetask;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DroneServiceTaskApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
